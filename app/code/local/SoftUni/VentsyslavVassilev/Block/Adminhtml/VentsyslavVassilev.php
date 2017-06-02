351858f95999da0133e9430441792cb7a9a195bc
wencywww <wency.www@gmail.com>
2017-06-02 19:39:35 +0300

    Update on some translations

--- app/code/local/SoftUni/VentsyslavVassilev/Block/Adminhtml/VentsyslavVassilev.php
+++ app/code/local/SoftUni/VentsyslavVassilev/Block/Adminhtml/VentsyslavVassilev.php
@@ -13,8 +13,8 @@ class SoftUni_VentsyslavVassilev_Block_Adminhtml_VentsyslavVassilev extends Mage
 
         $this->_blockGroup = 'softuni_ventsyslavvassilev';
         $this->_controller = 'adminhtml_ventsyslavvassilev';
-        $this->_headerText = Mage::helper('cms')->__('Submissions');
-        $this->_addButtonLabel = Mage::helper('cms')->__('Add Submission');
+        $this->_headerText = Mage::helper('softuni_ventsyslavvassilev')->__('Submissions');
+        $this->_addButtonLabel = Mage::helper('softuni_ventsyslavvassilev')->__('Add Submission');
         parent::__construct();
     }
 }
