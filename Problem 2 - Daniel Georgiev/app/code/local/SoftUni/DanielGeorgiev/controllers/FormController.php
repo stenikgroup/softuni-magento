caf230e439ca7103c679c341f75117ab5719dd75
Daniel Georgiev <danield.georgiev@gmail.com>
2017-06-05 18:44:26 +0300

    Homework 3 - solution

--- Problem 2 - Daniel Georgiev/app/code/local/SoftUni/DanielGeorgiev/controllers/FormController.php
+++ Problem 2 - Daniel Georgiev/app/code/local/SoftUni/DanielGeorgiev/controllers/FormController.php
@@ -10,15 +10,17 @@ class SoftUni_DanielGeorgiev_FormController extends Mage_Core_Controller_Front_A
 
     public function postAction()
     {
-        $submission = Mage::getModel('softuni_danielgeorgiev/submission');
+//        if (!$this->_validateFormKey()) {
+//            return $this->_redirect('*/*/');
+//        }
 
+//        $post = Mage::app()->getRequest()->getPost();
+        $submission = Mage::getModel('softuni_danielgeorgiev/submission');
         $submission->setSubmissionName(Mage::app()->getRequest()->getPost('dg-submission-name'));
         $submission->setSubmissionAge(Mage::app()->getRequest()->getPost('dg-submission-age'));
         $submission->setSubmissionTelephone(Mage::app()->getRequest()->getPost('dg-submission-telephone'));
         $submission->setSubmissionEmail(Mage::app()->getRequest()->getPost('dg-submission-email'));
-
         $submission->save();
-
         $this->_redirectReferer();
     }
 }
---
+++ app/code/local/SoftUni/DanielGeorgiev/Block/Adminhtml/Danielgeorgiev/Edit.php
@@ -0,0 +1,46 @@
+<?php
+
+class SoftUni_DanielGeorgiev_Block_Adminhtml_Danielgeorgiev_Edit extends Mage_Adminhtml_Block_Widget_Form_Container
+{
+    public function __construct()
+    {
+        $this->_objectId = 'submission_id';
+        $this->_blockGroup = 'softuni_danielgeorgiev';
+        $this->_controller = 'adminhtml_danielgeorgiev';
+
+        parent::__construct();
+
+        $this->_updateButton('save', 'label', Mage::helper('softuni_submission')->__('Save Submission'));
+        $this->_updateButton('delete', 'label', Mage::helper('softuni_submission')->__('Delete Submission'));
+
+        $this->_addButton('saveandcontinue', array(
+            'label'     => Mage::helper('adminhtml')->__('Save and Continue Edit'),
+            'onclick'   => 'saveAndContinueEdit()',
+            'class'     => 'save',
+        ), -100);
+
+        $this->_formScripts[] = "
+            function toggleEditor() {
+                if (tinyMCE.getInstanceById('block_content') == null) {
+                    tinyMCE.execCommand('mceAddControl', false, 'block_content');
+                } else {
+                    tinyMCE.execCommand('mceRemoveControl', false, 'block_content');
+                }
+            }
+
+            function saveAndContinueEdit(){
+                editForm.submit($('edit_form').action+'back/edit/');
+            }
+        ";
+    }
+
+    public function getHeaderText()
+    {
+        if (Mage::registry('softuni_danielgeorgiev_submission')->getId()) {
+            return Mage::helper('softuni_danielgeorgiev')->__("Edit Submission '%s'", $this->escapeHtml(Mage::registry('softuni_danielgeorgiev_submission')->getSubmissionName()));
+        }
+        else {
+            return Mage::helper('softuni_danielgeorgiev')->__('New Submission');
+        }
+    }
+}
