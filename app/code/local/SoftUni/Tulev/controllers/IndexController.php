b7384b9ebee92344a27bb24b9d00e62a35678d65
2Lev <georgetulev@abv.bg>
2017-06-06 17:25:55 +0300

    Adding some translations

--- app/code/local/SoftUni/Tulev/controllers/IndexController.php
+++ app/code/local/SoftUni/Tulev/controllers/IndexController.php
@@ -14,6 +14,14 @@ class SoftUni_Tulev_IndexController extends Mage_Core_Controller_Front_Action {
         $post = Mage::app()->getRequest()->getPost();
         $submission = Mage::getModel('softuni_tulev/submission');
 
+        try {
+            Mage::getSingleton('core/session')->addSuccess(
+                $this->__("Your submission has been saved successfully")
+            );
+        } catch (Exception $e) {
+            Mage::getSingleton('core/session')->addError($e->getMessage());
+        }
+
         $submission->setName($post['name']);
         $submission->setAge($post['age']);
         $submission->setPhone($post['phone']);
